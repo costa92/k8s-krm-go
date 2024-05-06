fece0e2eb4527e4d50394d6b38221ddb1a9102e9
costa92 <944310536@qq.com>
2024-05-07 00:03:39 +0800

    feat: add options

--- scripts/make-rules/tools.mk
+++ scripts/make-rules/tools.mk
@@ -10,3 +10,26 @@ _install.mockgen: ## Install mockgen which is a tool used to generate mock code.
 .PHONY: _install.cfssl
 _install.cfssl: ## Install cfssl toolkit.
 	@$(SCRIPTS_DIR)/install.sh krm::install::install_cfssl
+
+
+.PHONY: _install.grpc
+_install.grpc:
+	@$(GO) install google.golang.org/protobuf/cmd/protoc-gen-go@$(PROTOC_GEN_GO_VERSION)
+	@$(GO) install google.golang.org/grpc/cmd/protoc-gen-go-grpc@$(PROTOC_GEN_GO_GRPC_VERSION)
+	@$(GO) install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@$(GRPC_GATEWAY_VERSION)
+	@$(GO) install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@$(GRPC_GATEWAY_VERSION)
+	@$(SCRIPTS_DIR)/install-protoc.sh
+
+.PHONY: _install.kratos
+_install.kratos: _install.grpc ## Install kratos toolkit, includes multiple protoc plugins.
+	@$(GO) install github.com/joelanford/go-apidiff@$(GO_APIDIFF_VERSION)
+	@$(GO) install github.com/envoyproxy/protoc-gen-validate@$(PROTOC_GEN_VALIDATE_VERSION)
+	@$(GO) install github.com/google/gnostic/cmd/protoc-gen-openapi@$(PROTOC_GEN_OPENAPI_VERSION)
+	@$(GO) install github.com/go-kratos/kratos/cmd/kratos/v2@$(KRATOS_VERSION)
+	@$(GO) install github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2@$(KRATOS_VERSION)
+	@$(GO) install github.com/go-kratos/kratos/cmd/protoc-gen-go-errors/v2@$(KRATOS_VERSION)
+	@$(SCRIPTS_DIR)/add-completion.sh kratos bash
+
+.PHONY: _install.grpcurl
+_install.grpcurl:
+	@$(GO) install github.com/fullstorydev/grpcurl/cmd/grpcurl@$(GRPCURL_VERSION)
